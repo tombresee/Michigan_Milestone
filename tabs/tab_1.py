0eb9c9e58f7cfede3b2d4c698686d290f215c21a
Tom Bresee <tbresee@umich.edu>
2021-09-13 18:00:38 -0500

    -

--- tabs/tab_1.py
+++ tabs/tab_1.py
@@ -1,7 +1,9 @@
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+
 
 tab_1_layout = html.Div([
     html.H1('Page 1'),
