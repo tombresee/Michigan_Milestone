b86cb5540345320e8f04a02976c0057152b3f168
Tom Bresee <tbresee@umich.edu>
2021-09-13 18:00:03 -0500

    -

--- app.py
+++ app.py
@@ -2,74 +2,79 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+from tabs import tab_1
+from tabs import tab_2
+from tabs import tab_3
 
+########### Define your variables ######
 
-########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='darkred'
-color2='orange'
-mytitle='ML Tactics'
-tabtitle='sensor_ML'
-myheading='Sensor Data Analysis'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
-
-########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
-
-beer_data = [bitterness, alcohol]
-
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
-
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
-
-
+myheading1 = 'Plotly Dash -- multiple tabs'
+tabtitle = 'dash tabs'
+sourceurl = 'https://dash.plot.ly/dash-core-components/tabs'
+githublink = 'https://github.com/austinlasseter/dash-multitab-simple'
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title=tabtitle
+app.config['suppress_callback_exceptions'] = True
+
 
 ########### Set up the layout
-app.layout = html.Div(children=[
-    
-    html.H3(myheading),
 
-    html.H4('Tactics for ML'),
+app.layout = html.Div([
+    html.H1(myheading1),
+    dcc.Tabs(id="tabs-example", value='tab-1-example',
+            children=[
+                dcc.Tab(label='Tab One', value='tab-1-example'),
+                dcc.Tab(label='Tab Two', value='tab-2-example'),
+                dcc.Tab(label='Tab Three', value='tab-3-example'),
+    ]),
+    html.Div([
+        html.Div(id='tabs-content-example'),
+    ], className='twelve columns',
+        style={'marginBottom': 50, 'marginTop': 25}),
+    html.Div([
+        html.A('Code on Github', href=githublink),
+        html.Br(),
+        html.A("Data Source", href=sourceurl),
+    ], className='twelve columns',
+        style={'textAlign':'right',
+                'fontColor':'#FFFFFF',
+                'backgroundColor':'#D3D3D3',})
+])
 
-    # dcc.Graph(
-    #     id='flyingdog',
-    #     figure=beer_fig
-    # ),
-    
-    html.A('Code on Github', href=githublink),
-    
-    html.Br(),
-    
-    html.A('Data Source', href=sourceurl),
-    
-    ])
+@app.callback(Output('tabs-content-example', 'children'),
+              [Input('tabs-example', 'value')])
+def render_content(tab):
+    if tab == 'tab-1-example':
+        return tab_1.tab_1_layout
+    elif tab == 'tab-2-example':
+        return tab_2.tab_2_layout
+    elif tab == 'tab-3-example':
+        return tab_3.tab_3_layout
+
+# Tab 1 callback
+@app.callback(dash.dependencies.Output('page-1-content', 'children'),
+              [dash.dependencies.Input('page-1-dropdown', 'value')])
+def page_1_dropdown(value):
+    return 'You have selected "{}"'.format(value)
+
+# Tab 2 callback
+@app.callback(Output('page-2-content', 'children'),
+              [Input('page-2-radios', 'value')])
+def page_2_radios(value):
+    return 'You have selected "{}"'.format(value)
+
+# Tab 3 callback
+@app.callback(Output('page-3-content', 'children'),
+              [Input('page-3-slider', 'value')])
+def page_3_slider(value):
+    return f'You have selected "{str(value)}"'
 
 
+############ Deploy
 if __name__ == '__main__':
-    app.run_server()
-
+    app.run_server(debug=True)
