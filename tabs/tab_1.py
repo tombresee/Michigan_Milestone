87df4fd0b49fcf8ec7e52c98ffe6bd20687bbe4d
Tom Bresee <tbresee@umich.edu>
2021-09-13 19:42:07 -0500

    -

--- tabs/tab_1.py
+++ tabs/tab_1.py
@@ -10,7 +10,16 @@ tab_1_layout = html.Div([
         html.Div([
             html.H6('Select one:'),
 
-            html.P("The following additional Python libaries are used by this dashboard:"),
+                                  html.Div(className='box-body',children=[
+                                    html.P("Space, and more specifically low-earth orbit, is about to get a whole lot busier and this is making many concerned. At present, there are about 2,000 operational satellites in low-earth orbit and more than double that in defunct satellites. But last year in October, SpaceX requested permission to launch 30,000 Starlink satellites into low-earth orbit. This is in addition to the 12,000 that already received approval. These satellites have already begun interrupting astronomical observations, creating light pollution and increasing collision risks in an environment where a collision could trigger a chain reaction which not only endangers current and future satellites but also human lives."),
+                                    html.P(children=[
+                                        "This dashboard consists of a few demonstrations of how congested space has become and where it is going.  This dashboard website is written in Python using ",
+                                        html.A(children="Plotly Dash",href="https://plotly.com/dash/", target="_blank"),
+                                        ".  The layout for the dashboard comes from ",
+                                        html.A(children="AdminLTE", href="https://adminlte.io/", target="_blank"),
+                                        ", a free open-source implemenation of Bootstrap. The data used to compile these visuals come from the following sources:"
+                                    ]), 
+                                    
 
             dcc.Dropdown(
                 id='page-1-dropdown',
@@ -23,6 +32,8 @@ tab_1_layout = html.Div([
                             ),
             ),
         ], className='four columns'),
+
+
         html.Div([
             html.H6(id='page-1-content')
         ], className='eight columns'),
