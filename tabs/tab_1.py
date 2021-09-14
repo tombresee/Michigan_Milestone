769d6dd96df0e94649fac183491ed25303111e51
Tom Bresee <tbresee@umich.edu>
2021-09-13 20:26:13 -0500

    -

--- tabs/tab_1.py
+++ tabs/tab_1.py
@@ -8,41 +8,8 @@ tab_1_layout = html.Div([
     html.H5('Introduction'),
     html.Div([
         html.Div([
-            html.H6('Select one:'),
-
-                                    html.P(
-                                        html.Ul(children=[
-                                            html.Li(children=[
-                                                html.A(href='https://www.space-track.org/',children="Space-Track.org", target="_blank"),
-                                                " - For providing the TLE data",
-                                            ]),
-                                            html.Li(children=[
-                                                html.A(href='https://celestrak.com/SOCRATES/',children="SOCRATES", target="_blank"),
-                                                " - For upcoming satellite collision probability detection",
-                                            ]),
-                                            html.Li(children=[
-                                                html.A(href='https://ntrs.nasa.gov/',children="NASA's History of On-Orbit Satellite Fragmentations", target="_blank"),
-                                                " - For satellite breakup dates",
-                                            ]),
-                                        ])
-                                    )
-
-
-            # dcc.Dropdown(
-            #     id='page-1-dropdown',
-            #     options=[{'label': i, 'value': i} for i in ['burger', 'fries', 'milkshake']],
-            #     value='burger',
-            #     style = dict(
-            #                 width = '70%',
-            #                 display = 'inline-block',
-            #                 verticalAlign = "middle"
-            #                 ),
-            # ),
-
-
+            html.H6('Select one:'),                     
         ], className='four columns'),
-     
-
         html.Div([
             html.H6(id='page-1-content')
         ], className='eight columns'),
