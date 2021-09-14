220e2af9701cd20add6a27152f53fb8fe6d66674
Tom Bresee <tbresee@umich.edu>
2021-09-13 21:42:02 -0500

    -

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def render_content(tab):
 
 # Tab 1 callback
 @app.callback(dash.dependencies.Output('page-1-content', 'children'),
-              dash.dependencies.Input('page-1-dropdown', 'value'))
+              [dash.dependencies.Input('page-1-dropdown', 'value')])
 def page_1_dropdown(value):
     return None
 
